92f420d5f62a774136040d27b0decbfb50f73ca1
freedom <freedom7341@hotmail.com>
2023-09-07 01:41:28 -0600

    initial commit

---
+++ clock/clock.c
@@ -0,0 +1,14 @@
+/* * * * * * * *\
+	CLOCK.C -
+		Copyright (c) 2023 Vortesys
+	DESCRIPTION -
+		Main file for the Clock application, containing
+		the wWinMain function.
+	LICENSE INFORMATION -
+		MIT License, see LICENSE.txt in the root folder
+\* * * * * * * */
+
+/* Headers */
+#include "clock.h"
+// #define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
---
+++ clock/clock.h
@@ -0,0 +1,14 @@
+/* * * * * * * *\
+	CLOCK.H -
+		Copyright (c) 2023 Vortesys
+	DESCRIPTION -
+		Primary header file for the Clock application.
+	LICENSE INFORMATION -
+		MIT License, see LICENSE.txt in the root folder
+\* * * * * * * */
+
+/* Pragmas */
+#pragma once
+
+/* Includes */
+#include <wtypes.h>
